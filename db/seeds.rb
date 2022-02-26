9c4ec827e88862f457c83a6f1dcaeb98ae65476b
k0va1 <al3xander.koval@gmail.com>
2022-02-26 18:46:41 +0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+ruby_category = PostCategory.create!(name: 'ruby')
+js_category = PostCategory.create!(name: 'js')
+java_category = PostCategory.create!(name: 'java')
