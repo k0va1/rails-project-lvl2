ff4472c7272858dcf135d4baef771672c75b2782
k0va1 <al3xander.koval@gmail.com>
2022-03-12 19:15:22 +0300

    fix tests

--- test/controllers/posts/comments_controller_test.rb
+++ test/controllers/posts/comments_controller_test.rb
@@ -4,19 +4,13 @@ require 'test_helper'
 
 module Posts
   class CommentsControllerTest < ActionDispatch::IntegrationTest
-    test 'should get index' do
-      get posts_comments_index_url
-      assert_response :success
-    end
+    test 'should create' do
+      sign_in(users(:one))
 
-    test 'should get new' do
-      get posts_comments_new_url
-      assert_response :success
-    end
-
-    test 'should get create' do
-      get posts_comments_create_url
-      assert_response :success
+      assert_difference('PostComment.count') do
+        post = posts(:one)
+        post post_comments_path(post), params: { post_comment: { content: 'content' }, format: 'turbo_stream' }
+      end
     end
   end
 end
--- test/controllers/posts/likes_controller_test.rb
+++ test/controllers/posts/likes_controller_test.rb
@@ -4,14 +4,23 @@ require 'test_helper'
 
 module Posts
   class LikesControllerTest < ActionDispatch::IntegrationTest
+    setup do
+      sign_in(users(:one))
+    end
+
     test 'should get create' do
-      get posts_likes_create_url
-      assert_response :success
+      assert_difference('PostLike.count') do
+        post = posts(:two)
+        post post_likes_path(post), params: { post_like: { post_id: post.id }, format: 'turbo_stream' }
+      end
     end
 
     test 'should get destroy' do
-      get posts_likes_destroy_url
-      assert_response :success
+      assert_difference('PostLike.count', -1) do
+        post = posts(:one)
+        like = post_likes(:one)
+        delete post_like_path(post, like), params: { format: 'turbo_stream' }
+      end
     end
   end
 end
